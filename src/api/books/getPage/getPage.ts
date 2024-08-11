51e4d99f64f65f7c2f5f586307d8b8feb90feba8
Colby Birkhead <birkheadc@gmail.com>
2024-08-11 13:20:00 +0900

    add type to getPage

--- src/api/books/getPage/getPage.ts
+++ src/api/books/getPage/getPage.ts
@@ -1,10 +1,14 @@
 import config from "@/config";
+import { Book } from "@/types/book/book";
+import { Paginated } from "@/types/paginated/paginated";
 
-export default async function getPage(paginationToken?: string) {
+const getPage = async (paginationToken?: string): Promise<Paginated<Book>> => {
   const url = `${config.api.API_URL}/books/page${paginationToken ? `/${paginationToken}` : ""}`;
 
   const response = await fetch(url, {
     method: "GET",
   });
   return response.json();
-}
+};
+
+export default getPage;
