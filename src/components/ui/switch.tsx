7eafc67a50f602a0ebd3b51b39830ea7c8ee5fd7
Colby Birkhead <birkheadc@gmail.com>
2024-08-11 19:48:47 +0900

    change switch styles

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -9,7 +9,7 @@ const Switch = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      "group peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-primary-700 dark:border-primary-200 transition-colors disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary-900 data-[state=unchecked]:bg-primary-50 dark:data-[state=checked]:bg-primary-300 dark:data-[state=unchecked]:bg-primary-900",
+      "group peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-primary-50 dark:border-primary-200 transition-colors disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary-900 data-[state=unchecked]:bg-primary-50 dark:data-[state=checked]:bg-primary-300 dark:data-[state=unchecked]:bg-primary-900",
       className
     )}
     {...props}
@@ -17,7 +17,7 @@ const Switch = React.forwardRef<
   >
     <SwitchPrimitives.Thumb
       className={cn(
-        "group-hover:bg-secondary-300 dark:group-hover:bg-secondary-500 pointer-events-none block h-5 w-5 rounded-full bg-primary-200 shadow-lg transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0 dark:bg-primary-600"
+        "group-hover:bg-secondary-500 group-focus:bg-secondary-500 pointer-events-none block h-5 w-5 rounded-full bg-primary-600 shadow-lg transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0 dark:bg-primary-600 text-primary-50"
       )}
     >
       {children}
