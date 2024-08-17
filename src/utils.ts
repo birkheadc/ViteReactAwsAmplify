b3889adb767f3e3508272e07af67525907a2d2a5
Colby Birkhead <birkheadc@gmail.com>
2024-08-17 10:38:16 +0900

    create extractAccessToken util function

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,26 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { AccessToken } from "@/types/accessToken/accessToken";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function extractAccessToken(data: unknown): AccessToken | null {
+  if (data == null) return null;
+  if (typeof data !== "object") return null;
+  if (!("AuthenticationResult" in data)) return null;
+
+  const AuthenticationResult = data.AuthenticationResult;
+
+  if (AuthenticationResult == null) return null;
+  if (typeof AuthenticationResult !== "object") return null;
+  if (!("AccessToken" in AuthenticationResult)) return null;
+
+  const AccessToken = AuthenticationResult.AccessToken;
+
+  if (AccessToken == null) return null;
+  if (typeof AccessToken !== "string") return null;
+
+  return AccessToken;
 }
